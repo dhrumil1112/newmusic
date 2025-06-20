57b6215c057042f07bea58187debb6218e3a4106
Dhrumil Pareshbhai Prajapati <BL0071@bliss.local>
2025-06-20 19:26:09 +0530

    signup success created

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,62 @@
+import {connect} from '@/dbConfig/dbConfig';
+import User from '@/models/usrModel';
+import { NextRequest, NextResponse } from 'next/server';
+import bcryptjs from 'bcryptjs';
+import { sendEmail } from '@/helpers/mailer';
+connect();
+
+export async function POST(request: NextRequest){
+    try {
+       const reqBody = await request.json()
+       const {username, email, password} = reqBody
+        // Validation
+        console.log(reqBody);
+        
+       const user = await User.findOne({email})
+        if (user) {
+            return NextResponse.json(
+                {
+                    error:  'User already exists with this email'
+                },
+                {
+                    status: 400
+                }
+            )
+        }
+
+        const salt = await bcryptjs.genSaltSync(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        })
+
+        const savedUser = await newUser.save()
+        console.log('savedUser', savedUser);
+        
+        // Send verification email
+        await sendEmail({
+            email,
+            emailType: 'VERIFY',
+            userID: savedUser._id
+        })
+
+        return NextResponse.json({
+            message: 'User Registered successfully. Please check your email to verify your account.',
+            success: true,
+            savedUser
+        })
+        
+    } catch (error:any) {
+        return NextResponse.json(
+            {
+                error: error.message
+            },
+            {
+                status: 500
+            }
+        )
+    }
+}
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,9 +1,50 @@
+import User from '@/models/usrModel';
 import nodemailer from 'nodemailer';
-
-export const sendEmail = async ({email, emailType, userId}) => {
+import bcryptjs from 'bcryptjs';
+export const sendEmail = async ({email, emailType, userId}:any) => {
     try {
-        
-    } catch (error) {
+        // TODO: configure mail for usage
+        const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+        if (emailType === 'VERIFY'){
+            await User.findByIdAndUpdate(userId,
+                {
+                    verifyToken: hashedToken,
+                    verifyTokenExpiry: Date.now() + 3600000 // 1 hour
+                }
+            )
+        } else if (emailType === 'RESET') {
+            await User.findByIdAndUpdate(userId,
+                {
+                    forgotPasswordToken: hashedToken,
+                    forgotPasswordTokenExpiry: Date.now() + 3600000 // 1 hour
+                }
+            )
+        }
+
+
+        // Create a test account or replace with real credentials.
+        // Looking to send emails in production? Check out our Email API/SMTP product!
+        var transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "b3762477838c64",
+                pass: "fc20a8efb0a5e7"
+            }
+        });
+        // Wrap in an async IIFE so we can use await.
+        const mailOptions = {
+            from: 'dhrumil@blisswebsolutions.com', // sender address
+            to: email,  // list of receivers
+            subject: emailType === 'VERIFY' ? "Verify your email" : "Reset your password", // Subject line
+            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === 'VERIFY' ? "Verify your email" : "Reset your password"} or copy and paste the link below in your brower.<br> ${process.env.DOMAIN}/verofyemail?token=${hashedToken} </p>`, // HTML body
+        }
+
+        const mailResponse = await transport.sendMail(mailOptions);
+        return mailResponse;
+
+    } catch (error:any) {
         
     }
 }
